37f7a13f70be77eff87dad8092ef7707db2c4907
jonathan <jonathan114311@gmail.com>
2026-05-04 19:36:00 -0500

    app web de imagenes aleatorias con api

--- script.js
+++ script.js
@@ -5,12 +5,28 @@ const nombreJugador = document.querySelector(".nombre-jugador");
 const btnCambiar = document.querySelector(".cambiar-btn");
 
 fetch("https://picsum.photos/v2/list")
-.then((response) => response.json())
-.then((jugadores) => console.log(jugadores))
-.then(()=> {
-    const fondo = jugadores.download_url
-    btnCambiar.addEventListener("clik",() => {
-        jugador.style.backgroundImage("fondo")
-    })
-})
-.catch(() => console.log("error"))
+  .then((response) => response.json())
+  .then((jugadores) => {
+    btnCambiar.addEventListener("click", () => {
+      const random = Math.floor(Math.random() * jugadores.length);
+      const fondo = jugadores[random].download_url;
+      const nombre = jugadores[random].author;
+
+      const img = new Image();
+      img.src = fondo;
+
+      img.onload = () => {
+        jugador.opacity = 0;
+        setTimeout(() => {
+          jugador.style.backgroundImage = `url(${fondo})`;
+          jugador.style.backgroundSize = "cover";
+          jugador.style.backgroundPosition = "center";
+          jugador.style.backgroundRepeat = "no-repeat";
+          nombreJugador.textContent = nombre;
+          jugador.opacity = 1;
+        }, 200);
+      };
+    });
+  })
+
+  .catch(() => console.log("error"));
